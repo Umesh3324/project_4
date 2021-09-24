414228d528bc2f590a2c9265feb801bcb9179525
Umesh3324 <umesh.prajapati297@gmail.com>
2021-09-24 17:25:21 +0530

    This is comment.

---
+++ project_4/src/main/java/in/co/rays/project_4/bean/DropDownListBean.java
@@ -0,0 +1,23 @@
+package in.co.rays.project_4.bean;
+
+
+
+/**
+ * @author Umesh Prajapati
+ *
+ */
+public interface DropDownListBean {
+
+	
+	/**
+	 * return key of list element
+	 * @return key
+	 */
+	public String getKey();
+	
+	/**
+	 * display list of key element
+	 * @return value
+	 */
+	public String getValue();
+}
